0a56140c89460aae1e3606a32f5f044a55526f35
Tharin Taechanadit <veekungx@gmail.com>
2020-05-23 02:53:40 +0700

    add ParkingLotController -> issueTicket() test

--- src/modules/parking-lot/parking-lot.controller.spec.ts
+++ src/modules/parking-lot/parking-lot.controller.spec.ts
@@ -3,10 +3,12 @@ import { CommandBus } from '@nestjs/cqrs';
 import { CarSize } from '../../../src/car';
 import { ParkingLotController } from './parking-lot.controller';
 import { CreateParkingLotCommand } from './create-parking-lot.command';
+import { IssueTicketCommand } from './issue-ticket.command';
 
 describe('ParkingLotController', () => {
   const mockCommandBus = { execute: jest.fn() };
   let parkingLotController: ParkingLotController;
+
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [ParkingLotController],
@@ -16,13 +18,17 @@ describe('ParkingLotController', () => {
     parkingLotController = moduleRef.get<ParkingLotController>(
       ParkingLotController,
     );
+
+    jest.clearAllMocks();
   });
 
   describe('create', () => {
-    it('should execute CreateParkingLotCommand', async () => {
+    it(`should execute ${CreateParkingLotCommand.name}`, async () => {
       const numOfSlots = 100;
-      await parkingLotController.create(numOfSlots);
       const command = new CreateParkingLotCommand(numOfSlots);
+
+      await parkingLotController.create(numOfSlots);
+
       expect(mockCommandBus.execute).toHaveBeenCalledTimes(1);
       expect(mockCommandBus.execute).toHaveBeenCalledWith(command);
     });
@@ -30,7 +36,38 @@ describe('ParkingLotController', () => {
     it('should return status result', async () => {
       const response = { status: 'success' };
       mockCommandBus.execute.mockImplementation(() => response);
+
       const result = await parkingLotController.create(100);
+
+      expect(result).toBe(response);
+    });
+  });
+
+  describe('issueTicker', () => {
+    it(`should execute ${IssueTicketCommand.name}`, async () => {
+      const plateNumber = 'ABCD-123';
+      const carSize = CarSize.MEDIUM;
+      const command = new IssueTicketCommand(plateNumber, carSize);
+
+      await parkingLotController.issueTicket(plateNumber, carSize);
+
+      expect(mockCommandBus.execute).toHaveBeenCalledTimes(1);
+      expect(mockCommandBus.execute).toHaveBeenCalledWith(command);
+    });
+
+    it('should return information of plate number, car size and allocated parking slot', async () => {
+      const plateNumber = 'ABCD-123';
+      const carSize = CarSize.MEDIUM;
+      const slotNumber = 3;
+
+      const response = { plateNumber, carSize, slotNumber };
+      mockCommandBus.execute.mockImplementation(() => response);
+
+      const result = await parkingLotController.issueTicket(
+        plateNumber,
+        carSize,
+      );
+
       expect(result).toBe(response);
     });
   });
--- src/modules/parking-lot/parking-lot.controller.ts
+++ src/modules/parking-lot/parking-lot.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateParkingLotCommand } from './create-parking-lot.command';
+import { CarSize } from 'src/car';
+import { IssueTicketCommand } from './issue-ticket.command';
 
 @Controller('parking-lot')
 export class ParkingLotController {
@@ -12,4 +14,6 @@ export class ParkingLotController {
     const result = this.commandBus.execute(command);
     return result;
   }
+
+  issueTicket(plateNumber: string, carSize: CarSize) {}
 }
