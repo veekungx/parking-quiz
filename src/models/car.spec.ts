a44d3cbd3c21ffbcc6dd31f51c03c96ffefb142b
Tharin Taechanadit <veekungx@gmail.com>
2020-05-23 04:36:05 +0700

    add Car test

---
+++ src/models/car.spec.ts
@@ -0,0 +1,19 @@
+import { Car, CarSize } from './car';
+
+describe('Car', () => {
+  describe('Car.getPlateNumber()', () => {
+    it('should retrive plateNumber', () => {
+      const car = new Car('ABC-123', CarSize.LARGE);
+      const actual = car.getPlateNumber();
+      expect(actual).toEqual('ABC-123');
+    });
+  });
+  describe('Car.getCarSize()', () => {
+    it('should retrive carSize', () => {
+      const car = new Car('ABC-123', CarSize.LARGE);
+      const actual = car.getCarSize();
+
+      expect(actual).toEqual(CarSize.LARGE);
+    });
+  });
+});
