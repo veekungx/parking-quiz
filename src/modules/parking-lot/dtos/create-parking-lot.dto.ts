1317c0621d85de9601101971178ef5bf02dc1917
Tharin Taechanadit <veekungx@gmail.com>
2020-05-23 11:17:07 +0700

    improve API document property

--- src/modules/parking-lot/dtos/create-parking-lot.dto.ts
+++ src/modules/parking-lot/dtos/create-parking-lot.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateParkingLotDto {
+  @ApiProperty({ minimum: 1, default: 100 })
   numOfSlots: number;
 }
--- src/modules/parking-lot/dtos/issue-ticket.dto.ts
+++ src/modules/parking-lot/dtos/issue-ticket.dto.ts
@@ -1,6 +1,10 @@
 import { CarSize } from '../../../models/car';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class IssueTicketDto {
+  @ApiProperty()
   plateNumber: string;
+
+  @ApiProperty({ enum: ['SMALL', 'MEDIUM', 'LARGE'] })
   carSize: CarSize;
 }
