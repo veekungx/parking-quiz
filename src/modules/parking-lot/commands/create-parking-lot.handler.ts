d6079b059c1d3413909bd534c2e19fd42742c132
Tharin Taechanadit <veekungx@gmail.com>
2020-05-23 07:44:49 +0700

    improve CreateParkingLotHandler

--- src/modules/parking-lot/commands/create-parking-lot.handler.ts
+++ src/modules/parking-lot/commands/create-parking-lot.handler.ts
@@ -1,15 +1,21 @@
 import { CommandHandler, ICommandHandler, EventPublisher } from '@nestjs/cqrs';
 import { CreateParkingLotCommand } from './create-parking-lot.command';
 import { ParkingLot } from '../../../models/parking-lot';
+import { ParkingLotRepository } from '../repositories/parking-lot.repository';
 
 @CommandHandler(CreateParkingLotCommand)
 export class CreateParkingLotHandler
   implements ICommandHandler<CreateParkingLotCommand> {
-  constructor(private publisher: EventPublisher) {}
+  constructor(
+    private readonly publisher: EventPublisher,
+    private readonly parkingLotRepository: ParkingLotRepository,
+  ) {}
   async execute(command: CreateParkingLotCommand) {
     const { numOfSlots } = command;
 
-    const parkingLot = this.publisher.mergeObjectContext(new ParkingLot());
+    const parkingLot = this.publisher.mergeObjectContext(
+      await this.parkingLotRepository.create(),
+    );
     parkingLot.createParkingLot(numOfSlots);
     parkingLot.commit();
 
