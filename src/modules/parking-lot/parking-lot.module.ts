a676ea6c35fcd42111fb3b2e5e294591de84e635
Tharin Taechanadit <veekungx@gmail.com>
2020-05-23 15:31:49 +0700

    inject SlotRepository and QueryHandlers

--- src/modules/parking-lot/parking-lot.module.ts
+++ src/modules/parking-lot/parking-lot.module.ts
@@ -4,10 +4,18 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { ParkingLotRepository } from './repositories/parking-lot.repository';
 import { CommandHandlers } from './commands';
 import { EventHandlers } from './events';
+import { SlotRespository } from './repositories/slot.repository';
+import { QueryHandlers } from './queries';
 
 @Module({
   imports: [CqrsModule],
   controllers: [ParkingLotController],
-  providers: [ParkingLotRepository, ...CommandHandlers, ...EventHandlers],
+  providers: [
+    ParkingLotRepository,
+    SlotRespository,
+    ...CommandHandlers,
+    ...EventHandlers,
+    ...QueryHandlers,
+  ],
 })
 export class ParkingLotModule {}
